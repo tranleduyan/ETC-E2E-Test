f81a393baa612145d0b7d4f93ee265e1beafffa2
YiJiePV <qiank@spu.edu>
2024-04-25 21:54:50 -0700

    Refactoring existing dashboard tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -416,7 +416,8 @@ describe("Sign Up Page", () => {
 
   it("Failed Sign Up with Existing Account (email)", () => {
     /** Verify and enter temp SPU email into school email input field */
-    cy.get("input[name='emailAddress'").should("exist").type("user_test@spu.edu");
+    cy.get("input[name='emailAddress'").as('emailbox').should("exist");
+    cy.get("@emailbox").type("user_test@spu.edu");
     
     /** Verify if "Continue" button */
     cy.contains("Continue").should("exist").click();
@@ -491,13 +492,10 @@ describe("Sign Up Page", () => {
   })
 })
 
-describe("Dashboard Page", () => {
+describe("Admin Dashboard & Functionalities", () => {
   beforeEach(() => {
     cy.visit(home);
-  });
 
-  it("Dashboard for Admin", () => {
-    //#region Sign In with Admin Credentials
     /** Verify if the input field for school email */
     cy.get("input[name='emailAddress'").should("exist").type("admin_test@spu.edu");
 
@@ -512,9 +510,9 @@ describe("Dashboard Page", () => {
 
     /** Verify if user is navigated to Dashboard */
     cy.url().should("include", "/Dashboard");
-    //#endregion
+  });
 
-    //#region Verify Admin Specifications
+  it("Admin Dashboard Specifications", () => {
     /** Verify if the inventory list exists */
     cy.get(".AdminDashboard-InventorySection").should("exist");
 
@@ -524,11 +522,59 @@ describe("Dashboard Page", () => {
     /** Verify if the "Add Equipment" button exists and functionality works*/
     cy.get(".AdminDashboard-AddEquipmentButton").should("exist").click();
     cy.url().should("include", "/AddToInventory");
-    //#endregion
   });
 
-  it("Dashboard for Faculty", () => {
-    //#region Sign In with Faculty Credentials
+  it("Navigation Bar for Admin", () => {
+    //#region Verify if the admin-specific buttons exist and work correctly
+    /** Verify Inventory Navigation */
+    cy.get(".NavigationBarButton-InventoryButton").as('invenbtn').should("exist");
+    cy.get("@invenbtn").click();
+    cy.url().should("include", "/Inventory");
+
+    /** Verify Add To Inventory Navigation */
+    cy.get(".NavigationBarButton-AddEquipmentButton").as('addeqbtn').should("exist");
+    cy.get("@addeqbtn").click();
+    cy.url().should("include", "/AddToInventory");
+    
+    /** Verify Users Navigation */
+    cy.get(".NavigationBarButton-UsersButton").as('usersbtn').should("exist");
+    cy.get("@usersbtn").click();
+    cy.url().should("include", "/Users");
+    //#endregion
+
+    //#region Verify if the general buttons exist and work correctly
+    /** Verify Dashboard Navigation */
+    cy.get(".NavigationBarButton-DashboardButton").as('dashbtn').should("exist");
+    cy.get("@dashbtn").click();
+    cy.url().should("include", "/Dashboard");
+
+    /** Verify Reservations Navigation */
+    cy.get(".NavigationBarButton-ReservationsButton").as('resbtn').should("exist");
+    cy.get("@resbtn").click();
+    cy.url().should("include", "/Reservations");
+
+    /** Verify Notifications Navigation */
+    cy.get(".NavigationBarButton-NotificationsButton").as('notifybtn').should("exist");
+    cy.get("@notifybtn").click();
+    cy.url().should("include", "/Notifications");
+
+    /** Verify Settings Navigation */
+    cy.get(".NavigationBarButton-SettingsButton").as('setbtn').should("exist");
+    cy.get("@setbtn").click();
+    cy.url().should("include", "/Settings");
+
+    /** Verify Sign Out Navigation */
+    cy.get(".NavigationBarButton-SignOutButton").as('signoutbtn').should("exist");
+    cy.get("@signoutbtn").click();
+    cy.url().should("eq", 'http://localhost:3000/');
+    //#endregion
+  });
+});
+
+describe("Faculty Dashboard & Functionalities", () => {
+  beforeEach(() => {
+    cy.visit(home);
+
     /** Verify if the input field for school email */
     cy.get("input[name='emailAddress'").should("exist").type("faculty_test@spu.edu");
 
@@ -543,9 +589,9 @@ describe("Dashboard Page", () => {
 
     /** Verify if user is navigated to Dashboard */
     cy.url().should("include", "/Dashboard");
-    //#endregion
+  });
 
-    //#region Verify Faculty-Dashboard Specified
+  it("Faculty-Dashboard Specified", () => {
     /** Verify if the Equipment Usage Section that contains two filters - "Currently Using" and "Recently Used" exist */
     cy.get(".FacultyDashboard-EquipmentFilterSection").should("exist");
     cy.get(".FacultyDashboard-EquipmentFilterCardList").should("exist");
@@ -555,11 +601,40 @@ describe("Dashboard Page", () => {
     /** Verify if the "Make Reservation" exists and functionality works */
     cy.get(".FacultyDashboard-MakeReservationButton").should("exist").click();
     cy.url().should("include", "/Reservations");
-    //#endregion
   });
 
-  it("Dashboard for Student", () => {
-    //#region Sign In with Student Credentials
+  it("Navigation Bar for Faculty", () => {
+    /** Verify Dashboard Navigation */
+    cy.get(".NavigationBarButton-DashboardButton").as('dashbtn').should("exist");
+    cy.get("@dashbtn").click();
+    cy.url().should("include", "/Dashboard");
+
+    /** Verify Reservations Navigation */
+    cy.get(".NavigationBarButton-ReservationsButton").as('resbtn').should("exist");
+    cy.get("@resbtn").click();
+    cy.url().should("include", "/Reservations");
+
+    /** Verify Notifications Navigation */
+    cy.get(".NavigationBarButton-NotificationsButton").as('notifybtn').should("exist");
+    cy.get("@notifybtn").click();
+    cy.url().should("include", "/Notifications");
+
+    /** Verify Settings Navigation */
+    cy.get(".NavigationBarButton-SettingsButton").as('setbtn').should("exist");
+    cy.get("@setbtn").click();
+    cy.url().should("include", "/Settings");
+
+    /** Verify Sign Out Navigation */
+    cy.get(".NavigationBarButton-SignOutButton").as('signoutbtn').should("exist");
+    cy.get("@signoutbtn").click();
+    cy.url().should("eq", 'http://localhost:3000/');
+  });
+})
+
+describe("Student Dashboard & Functionalities", () => {
+  beforeEach(() => {
+    cy.visit(home);
+
     /** Verify if the input field for school email */
     cy.get("input[name='emailAddress'").should("exist").type("student_test@spu.edu");
 
@@ -574,9 +649,9 @@ describe("Dashboard Page", () => {
 
     /** Verify if user is navigated to Dashboard */
     cy.url().should("include", "/Dashboard");
-    //#endregion
+  });
 
-    //#region Verify Student-Dashboard Specified
+  it("Dashboard for Student", () => {
     /** Verify if the Equipment Usage Section that contains two filters - "Currently Using" and "Recently Used" exist */
     cy.get(".StudentDashboard-EquipmentFilterSection").should("exist");
     cy.get(".StudentDashboard-EquipmentFilterCardList").should("exist");
@@ -586,179 +661,32 @@ describe("Dashboard Page", () => {
     /** Verify if the "Make Reservation" exists and functionality works */
     cy.get(".StudentDashboard-MakeReservationButton").should("exist").click();
     cy.url().should("include", "/Reservations");
-    //#endregion
-  });
-});
-
-describe("Navigation Bar", () => {
-  beforeEach(() => {
-    cy.visit(home);
-  });
-
-  it("Navigation Bar for Admin", () => {
-    //#region Sign In with Admin Credentials
-    /** Verify if the input field for school email */
-    cy.get("input[name='emailAddress'").should("exist").type("admin_test@spu.edu");
-
-    /** Verify if "Continue" button */
-    cy.contains("Continue").should("exist").click();
-
-    /** Verify if the input field for password */
-    cy.get("input[name='password'").should("exist").type("test123");
-
-    /** Verify if "Sign In" button exists */
-    cy.contains("Sign In").should("exist").click();
-
-    /** Verify if user is navigated to Dashboard */
-    cy.url().should("include", "/Dashboard");
-    //#endregion
-
-    //#region Verify Admin-Navigation Bar Specified
-    /** Verify if the admin-specific buttons exist */
-    cy.get(".NavigationBarButton-InventoryButton").should("exist");
-    cy.get(".NavigationBarButton-AddEquipmentButton").should("exist");
-    cy.get(".NavigationBarButton-UsersButton").should("exist");
-
-    /** Verify if the general buttons exist */
-    cy.get(".NavigationBarButton-DashboardButton").should("exist");
-    cy.get(".NavigationBarButton-ReservationsButton").should("exist");
-    cy.get(".NavigationBarButton-NotificationsButton").should("exist");
-    cy.get(".NavigationBarButton-SettingsButton").should("exist");
-    cy.get(".NavigationBarButton-SignOutButton").should("exist");
-    //#endregion
-
-    //#region Verify All the Functionalities of Navigation Buttons
-    /** Verify Dashboard Navigation */
-    cy.get(".NavigationBarButton-DashboardButton").click();
-    cy.url().should("include", "/Dashboard");
-
-    /** Verify Reservations Navigation */
-    cy.get(".NavigationBarButton-ReservationsButton").click();
-    cy.url().should("include", "/Reservations");
-
-    /** Verify Notifications Navigation */
-    cy.get(".NavigationBarButton-NotificationsButton").click();
-    cy.url().should("include", "/Notifications");
-
-    /** Verify Settings Navigation */
-    cy.get(".NavigationBarButton-SettingsButton").click();
-    cy.url().should("include", "/Settings");
-
-    /** Verify Inventory Navigation */
-    cy.get(".NavigationBarButton-InventoryButton").click();
-    cy.url().should("include", "/Inventory");
-
-    /** Verify Add To Inventory Navigation */
-    cy.get(".NavigationBarButton-AddEquipmentButton").click();
-    cy.url().should("include", "/AddToInventory");
-
-    /** Verify Users Navigation */
-    cy.get(".NavigationBarButton-UsersButton").click();
-    cy.url().should("include", "/Users");
-
-    /** Verify Sign Out Navigation */
-    cy.get(".NavigationBarButton-SignOutButton").click();
-    cy.url().should("eq", 'http://localhost:3000/');
-    //#endregion
-  });
-
-  it("Navigation Bar for Faculty", () => {
-    //#region Sign In with Faculty Credentials
-    /** Verify if the input field for school email */
-    cy.get("input[name='emailAddress'").should("exist").type("faculty_test@spu.edu");
-
-    /** Verify if "Continue" button */
-    cy.contains("Continue").should("exist").click();
-
-    /** Verify if the input field for password */
-    cy.get("input[name='password'").should("exist").type("test123");
-
-    /** Verify if "Sign In" button exists */
-    cy.contains("Sign In").should("exist").click();
-
-    /** Verify if user is navigated to Dashboard */
-    cy.url().should("include", "/Dashboard");
-    //#endregion
-
-    //#region Verify Faculty-Navigation Bar Specified
-    /** Verify if the general/faculty-specific buttons exist */
-    cy.get(".NavigationBarButton-DashboardButton").should("exist");
-    cy.get(".NavigationBarButton-ReservationsButton").should("exist");
-    cy.get(".NavigationBarButton-NotificationsButton").should("exist");
-    cy.get(".NavigationBarButton-SettingsButton").should("exist");
-    cy.get(".NavigationBarButton-SignOutButton").should("exist");
-    //#endregion
-
-    //#region Verify All the Functionalities of Navigation Buttons
-    /** Verify Dashboard Navigation */
-    cy.get(".NavigationBarButton-DashboardButton").click();
-    cy.url().should("include", "/Dashboard");
-
-    /** Verify Reservations Navigation */
-    cy.get(".NavigationBarButton-ReservationsButton").click();
-    cy.url().should("include", "/Reservations");
-
-    /** Verify Notifications Navigation */
-    cy.get(".NavigationBarButton-NotificationsButton").click();
-    cy.url().should("include", "/Notifications");
-
-    /** Verify Settings Navigation */
-    cy.get(".NavigationBarButton-SettingsButton").click();
-    cy.url().should("include", "/Settings");
-
-    /** Verify Sign Out Navigation */
-    cy.get(".NavigationBarButton-SignOutButton").click();
-    cy.url().should("eq", 'http://localhost:3000/');
-    //#endregion
   });
 
   it("Navigation Bar for Student", () => {
-    //#region Sign In with Student Credentials
-    /** Verify if the input field for school email */
-    cy.get("input[name='emailAddress'").should("exist").type("student_test@spu.edu");
-
-    /** Verify if "Continue" button */
-    cy.contains("Continue").should("exist").click();
-
-    /** Verify if the input field for password */
-    cy.get("input[name='password'").should("exist").type("test123");
-
-    /** Verify if "Sign In" button exists */
-    cy.contains("Sign In").should("exist").click();
-
-    /** Verify if user is navigated to Dashboard */
-    cy.url().should("include", "/Dashboard");
-    //#endregion
-
-    //#region Verify Student-Sign In Specified
-    /** Verify if the general/student-specific buttons exist */
-    cy.get(".NavigationBarButton-DashboardButton").should("exist");
-    cy.get(".NavigationBarButton-ReservationsButton").should("exist");
-    cy.get(".NavigationBarButton-NotificationsButton").should("exist");
-    cy.get(".NavigationBarButton-SettingsButton").should("exist");
-    cy.get(".NavigationBarButton-SignOutButton").should("exist");
-    //#endregion
-
-    //#region Verify All the Functionalities of Navigation Buttons
     /** Verify Dashboard Navigation */
-    cy.get(".NavigationBarButton-DashboardButton").click();
+    cy.get(".NavigationBarButton-DashboardButton").as('dashbtn').should("exist");
+    cy.get("@dashbtn").click();
     cy.url().should("include", "/Dashboard");
 
     /** Verify Reservations Navigation */
-    cy.get(".NavigationBarButton-ReservationsButton").click();
+    cy.get(".NavigationBarButton-ReservationsButton").as('resbtn').should("exist");
+    cy.get("@resbtn").click();
     cy.url().should("include", "/Reservations");
 
     /** Verify Notifications Navigation */
-    cy.get(".NavigationBarButton-NotificationsButton").click();
+    cy.get(".NavigationBarButton-NotificationsButton").as('notifybtn').should("exist");
+    cy.get("@notifybtn").click();
     cy.url().should("include", "/Notifications");
 
     /** Verify Settings Navigation */
-    cy.get(".NavigationBarButton-SettingsButton").click();
+    cy.get(".NavigationBarButton-SettingsButton").as('setbtn').should("exist");
+    cy.get("@setbtn").click();
     cy.url().should("include", "/Settings");
 
     /** Verify Sign Out Navigation */
-    cy.get(".NavigationBarButton-SignOutButton").click();
+    cy.get(".NavigationBarButton-SignOutButton").as('signoutbtn').should("exist");
+    cy.get("@signoutbtn").click();
     cy.url().should("eq", 'http://localhost:3000/');
-    //#endregion
   });
 });
